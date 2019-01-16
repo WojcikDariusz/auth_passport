5f91823f86ff2f7b3b3629ddb43c589d185b7f14
Dariusz wojcik <dariusz.wojcik@op.pl>
2019-01-16 16:27:23 +0100

    First commit

---
+++ app.js
@@ -0,0 +1,76 @@
+var express               = require("express"),
+    mongoose              = require("mongoose"),
+    passport              = require("passport"),
+    bodyParser            = require("body-parser"),
+    User                  = require("./models/user"),
+    LocalStrategy         = require("passport-local"),
+    passportLocalMongoose = require("passport-local-mongoose")
+    
+mongoose.connect("mongodb://localhost/authorization_app");
+var app = express();
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(require("express-session")({
+    secret: "Auth is the best thing",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.set('view engine', 'ejs');
+
+//ROUTES
+
+app.get("/", function(req,res) {
+   res.render("home"); 
+});
+
+app.get("/secret", function(req,res) {
+    res.render("secret");
+});
+
+//REGISTRATION
+app.get("/register", function(req,res) {
+
+   res.render("registration");
+});
+
+app.post("/register", function(req,res) {
+
+  User.register(new User({username : req.body.username}), req.body.password, function(err,user) {
+     if(err) {
+         console.log(err);
+         res.render('register');
+     } 
+     passport.authenticate("local")(req,res, function() {
+        res.redirect("/secret"); 
+     });
+  });
+});
+
+//LOGIN
+app.get("/login", function(req,res){
+   res.render("login"); 
+});
+
+app.post("/login", passport.authenticate("local", {
+    successRedirect : "/secret",
+    failureRedirect : "/login"
+}) ,function(req,res){
+
+});
+
+//LOG-OUT
+
+
+app.listen(process.env.PORT,process.env.IP,function(){
+   console.log("Server started"); 
+});
